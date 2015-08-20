e10f92116c0274cba7c1e0dc2f7fc0b71bae2876
akandola <amanpreet123@gmail.com>
2015-08-20 02:14:19 -0400

    login page updates

--- 365/client/js/loginClient.js
+++ 365/client/js/loginClient.js
@@ -1,6 +1,29 @@
-//custom login/register functionas
-Template.login.events({
-	//login function
+Template.splash.events({
+	'click #backButton': function(){
+		console.log("clicked");
+		$("#firstSignPage").show();
+		$("#nextLoginButton").show();
+		$("#secondSignPage").hide();
+		$("#backButton").hide();
+		$("#splashTitle").css("margin-right", "0px");
+	}
+});
+
+Template.signup.events({
+	'click #facebookButton': function(){
+	  	Meteor.loginWithFacebook(
+	  		{requestPermissions: ['email', 'user_friends', 'user_location', 'user_status',
+				'user_posts']}, 
+			function(err){
+			    if (!err){
+			      Session.set("isFB", true);
+			      // $("#changePassword").hide();
+			    }
+			    else{
+			      console.log(err);
+			    }
+			}
+		)},
 	'submit form': function(event) {
 		event.preventDefault();
 		console.log("submit");
@@ -28,66 +51,13 @@ Template.login.events({
 				password: passwordVar,
 				DOB: DateOfBirth
 			});
-		}
-
-		// if ($("#passwordAgain").is(":visible"))
-		// {
-		// 	var emailVar = event.target.loginEmail.value;
-		// 	var passwordVar = event.target.loginPassword.value;
-		// 	var repeat = event.target.loginPasswordAgain.value;
-		// 	if (passwordVar == repeat){
-		// 		Accounts.createUser({
-		// 		    username: emailVar,
-		// 		    password: passwordVar
-		// 		});
-		// 	}
-		// 	else{
-		// 	//passwords do not match 
-		// 	}
-		// }
-		// else{
-		// 	var emailVar = event.target.loginEmail.value;
-		// 	var passwordVar = event.target.loginPassword.value;
-		// 	Meteor.loginWithPassword(emailVar, passwordVar, function(err){
-		// 		if (!err){
-		// 			Session.set("isFB", false);
-		// 		  // $("#changePassword").show();
-		// 		}
-		// 	});
-		// }        
-	},
-	//login with facebook
-	'click #login-buttons-facebook': function(){
-	  	Meteor.loginWithFacebook(
-	  		{requestPermissions: ['email', 'user_friends', 'user_location', 'user_status',
-				'user_posts']}, 
-			function(err){
-			    if (!err){
-			      Session.set("isFB", true);
-			      // $("#changePassword").hide();
-			    }
-			    else{
-			      console.log(err);
-			    }
-			}
-		)},
-	  //  if you hit create account button
-	'click #createAccount': function(){
-		if ($("#passwordAgain").is(":visible"))
-		{
-			$("#passwordAgain").hide();
-			$("#createAccount").text("Create Account");
-			$("#signIn").val("Sign In");
-		}
-		else{
-			$("#passwordAgain").show();
-			$("#createAccount").text("Cancel");
-			$("#signIn").val("Create");
-		}
+		}   
 	},
 	'click #nextLoginButton': function(){
-		var checkDate = isValidDate($("#dateOfBirth").val());
-		var name = $("#username").val();
+		// var checkDate = isValidDate($("#dateOfBirth").val());
+		// var name = $("#username").val();
+		var checkDate = true;
+		var name = true;
 		if (!name && checkDate){
 			alert("Please enter your name!");
 		}
@@ -95,14 +65,74 @@ Template.login.events({
 			$("#firstSignPage").hide();
 			$("#nextLoginButton").hide();
 			$("#secondSignPage").show();
+			$("#backButton").show();
+			$("#splashTitle").css("margin-right", "76px");
 		}
 	},
-	// 'click #createButton': function(){
-	// 	$("#firstSignPage").show();
-	// 	$("#nextLoginButton").show();
-	// 	$("#secondSignPage").hide();
-	// }
+	
 });
+
+// //custom login/register functionas
+// Template.login.events({
+// 	//login function
+// 	'submit form': function(event) {
+// 		event.preventDefault();
+// 		if ($("#passwordAgain").is(":visible"))
+// 		{
+// 			var emailVar = event.target.loginEmail.value;
+// 			var passwordVar = event.target.loginPassword.value;
+// 			var repeat = event.target.loginPasswordAgain.value;
+// 			if (passwordVar == repeat){
+// 				Accounts.createUser({
+// 				    username: emailVar,
+// 				    password: passwordVar
+// 				});
+// 			}
+// 			else{
+// 			//passwords do not match 
+// 			}
+// 		}
+// 		else{
+// 			var emailVar = event.target.loginEmail.value;
+// 			var passwordVar = event.target.loginPassword.value;
+// 			Meteor.loginWithPassword(emailVar, passwordVar, function(err){
+// 				if (!err){
+// 					Session.set("isFB", false);
+// 				  // $("#changePassword").show();
+// 				}
+// 			});
+// 		}        
+// 	},
+// 	//login with facebook
+// 	'click #login-buttons-facebook': function(){
+// 	  	Meteor.loginWithFacebook(
+// 	  		{requestPermissions: ['email', 'user_friends', 'user_location', 'user_status',
+// 				'user_posts']}, 
+// 			function(err){
+// 			    if (!err){
+// 			      Session.set("isFB", true);
+// 			      // $("#changePassword").hide();
+// 			    }
+// 			    else{
+// 			      console.log(err);
+// 			    }
+// 			}
+// 		)},
+// 	  //  if you hit create account button
+// 	'click #createAccount': function(){
+// 		if ($("#passwordAgain").is(":visible"))
+// 		{
+// 			$("#passwordAgain").hide();
+// 			$("#createAccount").text("Create Account");
+// 			$("#signIn").val("Sign In");
+// 		}
+// 		else{
+// 			$("#passwordAgain").show();
+// 			$("#createAccount").text("Cancel");
+// 			$("#signIn").val("Create");
+// 		}
+// 	}
+// });
 
 // Validates that the input string is a valid date formatted as "mm/dd/yyyy"
 function isValidDate(dateString)
