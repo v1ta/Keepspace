f7d6eef69aa39851c3a6153c3f363eb52c49abff
akandola <amanpreet123@gmail.com>
2015-09-15 00:30:47 -0400

    updated favicon

--- 365/server/kadira.js
+++ 365/server/kadira.js
@@ -1,2 +1,2 @@
 Kadira.connect('EsPdCASCHogEPAoZa', '0ce4966f-e7d6-4c48-87a0-d1256c2b9f5f');
-Kadira.connect('2ryZnXPy3vAsaEGwR', '4b02a1fa-407c-4e33-83e6-84a5471d6d59');
+// Kadira.connect('2ryZnXPy3vAsaEGwR', '4b02a1fa-407c-4e33-83e6-84a5471d6d59');
