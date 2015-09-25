e9065890962ed6c209c3dc6deade80393158085a
v1ta <jodvita@gmail.com>
2015-09-24 22:54:39 -0400

    merged mongo-refactor

--- 365/server/js/mail.js
+++ 365/server/js/mail.js
@@ -1,25 +1,5 @@
 Meteor.startup(function () {
-
   Accounts.emailTemplates.from = 'do-no-reply@mykeepspace.com';
-
-  
   Accounts.emailTemplates.siteName = 'mykeepspace';
-
-/*
-  Accounts.emailTemplates.verifyEmail.subject = function(user) {
-    return 'Confirm Your Email Address';
-  };
-
-  Accounts.emailTemplates.verifyEmail.text = function(user, url) {
-    return 'click on the following link to verify your email address: ' + url;
-
-  };
-*/
 });
 
-/*
-Accounts.config({
-	sendVerificationEmail: true,
-	forbidClientAccountCreation: false
-});
-*/
