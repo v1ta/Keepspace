621808bc76197564cb9314cf1efab52774c4f1f2
Joseph <jodvita@gmail.com>
2017-05-28 12:53:22 -0400

    frontend login routing

--- keepspace/static/js/index.js
+++ keepspace/static/js/index.js
@@ -3,16 +3,29 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import allReducers from './reducers/allReducers';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App';
 import Splash from './components/Splash';
 
-const loggedIn = false;
+const loggedIn = () => {
+    return true;
+};
 
-let page = loggedIn ? (<App/>) : (<Splash/>);
+const requireAuth = (nextState, replace) => {
+    if (loggedIn()) {
+        replace({
+            pathname: '/login'
+        });
+    }
+};
 
-ReactDOM.render(
+const routes = (
     <Provider store={createStore(allReducers)}>
-        {page}
-    </Provider>,
-    document.getElementById('main')
+        <Router history={browserHistory}>
+            <Route path="/" component={App} onEnter={requireAuth}/>
+            <Route path="login" component={Splash}/>
+        </Router>
+    </Provider>
 );
+
+ReactDOM.render(routes, document.getElementById('main'));
